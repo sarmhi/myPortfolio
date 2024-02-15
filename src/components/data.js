a58c42d29c6b5974ebffea811531e9f26a2e280f
sarmhi <sammiejay813@gmail.com>
2024-02-15 13:23:21 +0100

    added projects worked on last year

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,6 @@
 import tempown from "../assets/projects/tempown.png";
+import carry from "../assets/projects/carry.png";
+import pharmarx from "../assets/projects/pharmarx.png";
 import tempownEnt from "../assets/projects/tempownEnt.png";
 import imagePizzle from "../assets/projects/imagePizzle.png";
 import sammiUrl from "../assets/projects/sammiUrl.png";
@@ -7,12 +9,28 @@ import cinematics from "../assets/projects/cinematics.png";
 
 const data = [
   {
+    name: "Carry Africa",
+    image: carry,
+    stack: "React.js, Javascript, HTML5, CSS3, TailwindCss, Nest.js, Typescript, Typeorm, Postgres, Firebase, Tawk.io, Flutter, Google API",
+    link: "https://carry.africa/",
+    subtitle: "Logistics site",
+    description: `Carry provides co-ordinated and affordable customer services where customers get to experience seamless parcel delivery.`
+  },
+  {
     name: "Tempown",
     image: tempown,
     stack: "React.js, Javascript, HTML5, CSS3, TailwindCss, Node.js, Typescript, Mongoose, Mongodb",
     link: "https://www.tempown.com/",
     subtitle: "Peer-to-peer e-commerce site",
     description: `A peer-to-peer e-commerce site that connects lenders and borrowers of a particular item together for a small fee.`
+  },
+  {
+    name: "Pharmarx",
+    image: pharmarx,
+    stack: "React.js, Javascript, HTML5, CSS3, TailwindCss, Nest.js, Typescript, Mongoose, Mongodb",
+    link: "https://pharmarxng.com/",
+    subtitle: "Health E-commerce site",
+    description: `An e-commerce site that helps customers connect with a pharmacist as well as offers seemless purchase of health products.`
   },
   {
     name: 'Cinematics',
@@ -60,14 +78,5 @@ const data = [
     subtitle: "Peer-to-peer e-commerce site, Enterprise version",
     description: `The enterprise version of the peer-to-peer e-commerce site that connects lenders and borrowers of a particular item together for a small fee.`
   },
-  // {
-  //    "name": "Adela",
-  //   "image": adela,
-  //     "stack": "HTML5, CSS3, Javascript",
-  //   "link": "https://ty-codes.github.io/adela",
-  //   "subtitle": "Product landing page",
-  //    "description": "Landing page for Adela, a cosmetic company that provides skin and body care products. "
-  // },
-  
 ];
 export default data;
